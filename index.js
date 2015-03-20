f2dc7adc2f5b939ef3255ffecbc5c2273d1afa26
clarejacky <clarejacky@users.noreply.github.com>
2015-03-20 15:02:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
+function dayOfTheYear() {
+	var dayCount = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334];
+	var monthNumber = this.getMonth();
+	var month = dayCount[monthNumber];
+	var dayOfMonth = this.getDate();
+	var dayOfTheYear = month + dayOfMonth;
+	return dayOfTheYear;
+}
 function dayOfBirthday () {
 	var dayCount = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334];
 	var month = dayCount[6];
 	var dayOfBirthday = month + 24;
 	return dayOfBirthday;
 }
-
-console.log(dayOfBirthday);
+function daysUntilThirty (dayOfTheYear, dayOfBirthday) {
+	var birthday = dayOfBirthday();
+	var dayYear = dayOfTheYear();
+	var daysUntilThirty = birthday - dayYear;
+	return daysUntilThirty;
+}
+console.log("You will be 30 in "+daysUntilThirty()+" days!");
