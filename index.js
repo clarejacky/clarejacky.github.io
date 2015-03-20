24a3130133b186f561597f5ebfb1d368beb4e896
clarejacky <clarejacky@users.noreply.github.com>
2015-03-20 13:08:19 -0500

    Update index.js

--- index.js
+++ index.js
@@ -16,4 +16,5 @@ function daysUntilThirty (dayOfTheYear, dayOfBirthday) {
 	var daysUntilThirty = dayOfBirthday - dayOfTheYear;
 	return daysUntilThirty;
 	alert("You will be 30 in "+daysUntilThirty+" days!");
+	console.log(daysUntilThirty);
 }
