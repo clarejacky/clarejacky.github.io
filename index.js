c21319cfa06a862b61ef03f490f6616c3b523978
clarejacky <clarejacky@users.noreply.github.com>
2015-03-21 12:36:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function dayOfTheYear() {
 	var dayCount = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334];
-	var monthNumber = getMonth();
+	var monthNumber = this.getMonth();
 	var month = dayCount[monthNumber];
-	var dayOfMonth = getDate();
+	var dayOfMonth = this.getDate();
 	var dayOfTheYear = month + dayOfMonth;
 return dayOfTheYear;
 }
